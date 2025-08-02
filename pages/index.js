feb0160571533ef8aed3e2a0bac987acac0ae76f
rodrigosangali <84548693+rodrigosangali@users.noreply.github.com>
2025-08-02 15:48:59 +0000

    corrigido return da funcao index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-  turn(
+  return (
     <div>
       <h1>Pekelucha, meu primeiro pedaço de internet dedico a voce</h1>
       <img src="/image.gif" alt="GIF Exemplo" />
-    </div>,
+    </div>
   );
 }
 
