f69b3462c0e5cbaf91494825de5563ad4aecd96a
Rodrigo <rodrigosangali@gmail.com>
2025-08-30 11:21:48 -0300

    adicinado retorno json na mensagem

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,7 @@
 function status(request, response) {
-  response.status(200).send("numa relacao nao discuta com mulher");
+  response
+    .status(200)
+    .json({ mensagem: "numa relacao nao discuta com mulher" });
 }
 
 export default status;
