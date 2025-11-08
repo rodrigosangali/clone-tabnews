225768e0ef5171e549ff7fc582e92a79e5712f43
Rodrigo <rodrigo sangali@gmail.com>
2025-11-08 14:58:15 -0300

    implementado banco de dados via compose

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,11 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  console.log(response);
+
+  const responseBody = await response.json()
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdateAt)
+
 });
