288f39577f3083befb6e86d788c6f52e1d2ebd87
Rodrigo <rodrigo sangali@gmail.com>
2025-12-02 08:45:21 -0300

    commit

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,17 +2,41 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString()
-  const version_postgres = await database.query("SELECT version()")
-  // 1) Versão do Postgres
-  // 2) Conexoes máximas
-  // 3) Conexoes usadas
-  // 4) Incrementar os testes para cobrir as informaçoes acima
+
+  // const versionPostgresResult = await database.query("SELECT version()")
+  // const version_postgres = versionPostgresResult.rows[0].version
+
+  const versionPostgresResult = await database.query("SHOW server_version;")
+  const version_postgres = versionPostgresResult.rows[0].server_version
+
+  const maxConnectionsResult = await database.query("SHOW max_connections")
+  const max_connections = maxConnectionsResult.rows[0].max_connections
+
+  const databaseName = request.query.databaseName;
+  console.log(`Banco de dados selecionado : ${databaseName}`)
+  // Não é igual o java que soma string com +
+  //const usedConnetionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname ='" + databaseName + "';")
+  // Utilizando place holder
+  // const usedConnetionsResult = await database.query(`SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`)
+
+  const usedConnetionsResult = await database.query(`SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`)
+
+  const usedConnetions = usedConnetionsResult.rows[0].count
+
+  console.log(usedConnetions)
 
   response
     .status(200)
     .json({
-      updated_at: updateAt,
-      version_postgres: version_postgres.rows[0].version
+      depedencies: {
+        database: {
+          updated_at: updateAt,
+          version_postgres: version_postgres,
+          max_connections: max_connections,
+          used_connections: usedConnetions
+
+        },
+      },
     });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,11 +3,20 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json()
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.version_postgres).toBeDefined();
+  // expect(responseBody.updated_at).toBeDefined();
+  // expect(responseBody.version_postgres).toBeDefined();
+  // expect(responseBody.max_connections).toBeDefined();
+  // expect(responseBody.used_connetions).toBeDefined();
 
-  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdateAt)
-  expect(responseBody.version_postgres).toEqual('PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit')
+  const parseUpdateAt = new Date(responseBody.depedencies.database.updated_at).toISOString();
+  expect(responseBody.depedencies.database.updated_at).toEqual(parseUpdateAt)
+  expect(responseBody.depedencies.database.version_postgres).toEqual('16.0')
+  expect(responseBody.depedencies.database.max_connections).toEqual("100")
+  expect(responseBody.depedencies.database.used_connections).toEqual(1)
 
+});
+
+test.only("Acessar o banco de dados por inject Sql", async () => {
+  // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+  await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(1); --");
 });
