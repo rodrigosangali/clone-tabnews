0a66cdd4d15346346e07e89876bdb629e15d2b27
Rodrigo <rodrigo sangali@gmail.com>
2025-12-02 08:45:00 -0300

    slq injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,18 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString()
+  const version_postgres = await database.query("SELECT version()")
+  // 1) Versão do Postgres
+  // 2) Conexoes máximas
+  // 3) Conexoes usadas
+  // 4) Incrementar os testes para cobrir as informaçoes acima
+
   response
     .status(200)
-    .json({ updated_at: updateAt });
+    .json({
+      updated_at: updateAt,
+      version_postgres: version_postgres.rows[0].version
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,8 +4,10 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json()
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version_postgres).toBeDefined();
 
   const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdateAt)
+  expect(responseBody.version_postgres).toEqual('PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit')
 
 });
