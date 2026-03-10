443803273177f84de72da5a37fc2982a66ff9a6e
Rodrigo <rodrigosangali@gmail.com>
2026-03-10 08:49:06 -0300

    ultimas alteracoes

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,6 +1,13 @@
 import migrationRunner from "node-pg-migrate";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({})
+
+  console.log(process.env.development.DATABASE_URL)
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.development.DATABASE_URL,
+    dryRun: true,
+  });
+
   response.status(200).json(migrations);
+
 }
