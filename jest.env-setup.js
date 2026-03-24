42ac5bf4dc55691a1ea4a7294e76052a9b0c3615
Rodrigo <rodrigo sangali@gmail.com>
2026-03-24 11:56:10 -0300

    merge com master

---
+++ jest.env-setup.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv');
+
+// Força o carregamento do arquivo de desenvolvimento
+dotenv.config({
+    path: '.env.development'
+});
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,14 @@
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  }
+
+  );
+  expect(response.status).toBe(200);
+
+  const repondeBody = await response.json();
+
+  expect(Array.isArray(repondeBody)).toBe(true);
+
+  console.log(response);
+});
