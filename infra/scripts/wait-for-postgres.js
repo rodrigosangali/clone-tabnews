b4ed6e298e57e2e735e3ebe8c1f1a12a99fa3a28
Rodrigo <rodrigo sangali@gmail.com>
2026-05-15 09:07:05 -0300

    run prettier fix

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,16 +1,16 @@
 const { exec } = require("node:child_process");
 
 function checkPostgresConnection() {
-    exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
-    function handleReturn(error, stdout, stderr) {
-        if (stdout.search("accepting connections") === -1) {
-            process.stdout.write(".");
-            checkPostgresConnection();
-            return;
-        }
-        console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgresConnection();
+      return;
     }
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+  }
 }
 process.stdout.write("\n\n🔴 Waiting for PostgreSQL to be ready...");
 checkPostgresConnection();
