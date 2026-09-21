4441baf43c446c92995412ad780b082046e6ed90
Rodrigo <rodrigo sangali@gmail.com>
2026-09-21 12:18:03 -0300

    refactor: use the lint:prettier:fix

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -10,7 +10,6 @@ beforeAll(async () => {
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-
     test("Running pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
